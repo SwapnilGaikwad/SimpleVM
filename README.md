bf8115029266e284fa16f6084bace99f27292468
sgaikwad <gaikwad.swapnil@gmail.com>
2016-12-26 23:18:00 +0000

    Update README and add bytecodes that VM going to support

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SimpleVM
-Basic virtual machine for fun
+Basic virtual machine for fun.
+It is based on a tutorial by Terence Parr to build a simple virtual machine [1].
+
+
+[1] Tutorial Video: https://www.youtube.com/watch?v=OjaAToVkoTw
--- TestVM/src/vm/Bytecode.java
+++ TestVM/src/vm/Bytecode.java
@@ -2,4 +2,19 @@ package vm;
 
 public class Bytecode{
     public static final short IADD = 1;
+    public static final short ISUB = 2;
+    public static final short IMUL = 3;
+    public static final short ILT = 4;
+    public static final short IEQ = 5;
+    public static final short BR = 6;
+    public static final short BRT = 7;
+    public static final short BRF = 8;
+    public static final short ICONST = 9;
+    public static final short LOAD = 10;
+    public static final short GLOAD = 11;
+    public static final short STORE = 12;
+    public static final short GSTORE = 13;
+    public static final short PRINT = 14;
+    public static final short POP = 15;
+    public static final short HALT = 16;
 }
